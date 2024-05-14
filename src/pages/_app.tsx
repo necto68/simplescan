c37c1679922b1d3bbdf1170839193d9a6930fe1c
ikura-roe <110657185+ikura-roe@users.noreply.github.com>
2024-05-14 19:08:51 +0400

    feat: added vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@picocss/pico";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Simplescan</title>
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
